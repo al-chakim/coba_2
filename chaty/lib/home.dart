6e14d189cf7b4e03faa000b683c7cab5c91ed72a
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-01-27 22:47:55 +0700

    new my logic

---
+++ chaty/lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text('sdv', style: TextStyle(fontSize: 50),),
+          ],
+        ),
+      )
+    );
+  }
+}
