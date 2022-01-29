fe835aadc16092affda1afafa2a3db4b9d1ea550
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-01-29 08:19:25 +0700

    bg cl

--- chaty/lib/home.dart
+++ chaty/lib/home.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'theme.dart';
 
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: biruColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            Image.asset('assets/images/kono1.png')
-          ],
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset(
+                'assets/images/kono1.png',
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(height: 20,),
+              Text(
+                'Kazuma',
+                style: TextStyle(
+                  fontSize: 20, 
+                  color: putihColor
+                ),
+              ),
+            ],
+          ),
         ),
       )
     );
