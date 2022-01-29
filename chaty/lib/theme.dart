0ef1ec5fd8f1ac8e370bad7487ec410dddc1aa79
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-01-29 08:07:23 +0700

    textstyle

--- chaty/lib/theme.dart
+++ chaty/lib/theme.dart
@@ -5,3 +5,14 @@ Color putihColor = Color(0xffFFFFFF);
 Color abuColor = Color(0xff808BA2);
 Color hitamColor = Color(0xff2C3A59);
 Color hijauColor = Color(0xff29CB9E);
+
+TextStyle titleTextStyle = TextStyle(
+  color: hitamColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+
+TextStyle subtitleTextStyle = TextStyle(
+  color: hijauColor,
+  fontWeight: FontWeight.w300,
+);
