0d7d08b0401c74e4f188b8e3d99934f1f40ed982
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-01-29 08:02:57 +0700

    color

---
+++ chaty/lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color biruColor = Color(0xff00D1FF);
+Color putihColor = Color(0xffFFFFFF);
+Color abuColor = Color(0xff808BA2);
+Color hitamColor = Color(0xff2C3A59);
+Color hijauColor = Color(0xff29CB9E);
